c97694ab290416f40ecd3ba2b20d5e399532acee
Roi Mordechay <roi3363@yahoo.com>
2020-11-26 14:06:16 +0100

    code reformatting

--- agent.py
+++ agent.py
@@ -1,15 +1,13 @@
 from random import choice
 from kaggle_environments.envs.halite.helpers import *
 
-
-def agent(obs,config):
-
-    board = Board(obs,config)
+def agent(obs, config):
+    board = Board(obs, config)
     me = board.current_player
 
     # Set actions for each ship
     for ship in me.ships:
-        ship.next_action = choice([ShipAction.NORTH,ShipAction.EAST,ShipAction.SOUTH,ShipAction.WEST,None])
+        ship.next_action = choice([ShipAction.NORTH, ShipAction.EAST, ShipAction.SOUTH, ShipAction.WEST, None])
 
     # Set actions for each shipyard
     for shipyard in me.shipyards:
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from kaggle_environments import make
 
-env = make("halite", configuration={ "episodeSteps": 400 }, debug=True)
+env = make("halite", configuration={"episodeSteps": 400}, debug=True)
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    env.run(["agent.py", "random","random","random"])
+    env.run(["agent.py", "random", "random", "random"])
     return env.render(mode="html", width=800, height=600)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=5050)
-
-
